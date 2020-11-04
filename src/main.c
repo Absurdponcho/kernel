5d0b2233ceb1744de6e78535432d63ab9e546855
Absurdponcho <60100776+Absurdponcho@users.noreply.github.com>
2020-11-04 11:05:19 +1100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,28 +5,18 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
   EFI_STATUS Status;
   EFI_INPUT_KEY Key;
 
-  /* Store the system table for future use in other functions */
-  ST = SystemTable;
+  ST = SystemTable; //Store the system table
 
-  /* Say hi */
-  Status = ST->ConOut->OutputString(ST->ConOut, L"HelloWorld!\n\r");
+  Status = ST->ConOut->OutputString(ST->ConOut, L"HelloWorld!\n\r"); // Print HelloWorld to the console
 
+  if (EFI_ERROR(Status)) //If printing failed, return
+      return Status;
+
+  Status = ST->ConIn->Reset(ST->ConIn, FALSE); // Empty the Console Input Buffer
   if (EFI_ERROR(Status))
       return Status;
 
-  /* Now wait for a keystroke before continuing, otherwise your
-     message will flash off the screen before you see it.
+  while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ; // Poll For Key Input
 
-     First, we need to empty the console input buffer to flush
-     out any keystrokes entered before this point */
-  Status = ST->ConIn->Reset(ST->ConIn, FALSE);
-  if (EFI_ERROR(Status))
-      return Status;
-
-  /* Now wait until a key becomes available.  This is a simple
-     polling implementation.  You could try and use the WaitForKey
-     event instead if you like */
-  while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ;
-
-  return Status;
+  return Status; // Exit the UEFI application
 }
